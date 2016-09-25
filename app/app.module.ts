8eb392f8bbe305170db3fb09cef3c1f4e3772639
Artem <temchik2004@gmail.com>
2016-09-25 13:10:36 +0200

    Add routing; add search component; tuned routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders }  from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent, NavbarComponent ],
+  imports: [ BrowserModule, routing ],
+  declarations: [ AppComponent, NavbarComponent, SearchComponent, AboutComponent ],
+  providers: [ appRoutingProviders ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/app.routing.ts
@@ -0,0 +1,15 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
